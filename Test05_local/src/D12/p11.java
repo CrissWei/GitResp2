1e10e95304b2558a590b2922ee3ae826d401a347
CrissWei <iamcriss@qq.com>
2021-12-11 19:49:03 +0800

    p11第2次推送到GITHUB

--- Test05_local/src/D12/p11.java
+++ Test05_local/src/D12/p11.java
@@ -3,5 +3,9 @@ package D12;
 public class p11 {
     public static void main(String[] args) {
         System.out.println(123);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("新增commit啊");
     }
 }
